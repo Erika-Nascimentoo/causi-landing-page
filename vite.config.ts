6184dd5c5d3a918be05cde28a2f9021452fc774f
Vitor Garcia <vitor.garciamoreira@gmail.com>
2026-02-04 10:43:54 -0400

    Atualização para Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,6 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       host: '0.0.0.0',
-      proxy: {
-        '/api/webhook': {
-          target: 'https://n8n.aceleracaojuridica.com.br',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api\/webhook/, '/webhook-test/b08bb74b-0502-4e53-854f-7fedca4fa300'),
-        },
-      },
     },
     plugins: [react()],
     resolve: {
