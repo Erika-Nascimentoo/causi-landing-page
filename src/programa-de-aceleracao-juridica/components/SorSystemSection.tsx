c14e33148b2fa992f6a561ef185298cdd7b143ac
Vitor Garcia <webmaster@rino3.com.br>
2026-02-25 18:32:53 -0400

    style: highlight 'Plataforma Causi' in purple in the bonus card

--- src/programa-de-aceleracao-juridica/components/SorSystemSection.tsx
+++ src/programa-de-aceleracao-juridica/components/SorSystemSection.tsx
@@ -87,7 +87,7 @@ export const SorSystemSection: React.FC = () => {
                                 />
                                 <OfferListItem 
                                     isBonus
-                                    title="30 dias Grátis da Plataforma Causi no Plano Profissional"
+                                    title={<>30 dias Grátis da <span className="text-brand-primary">Plataforma Causi</span> no Plano Profissional</>}
                                 />
                             </ul>
                         </div>
